2d3f86694916be697b255775139766fe6eb9b75d
Luypaert <pierre.luypaert@hotmail.fr>
2021-06-22 00:12:02 +0200

    Co-authored-by: allanserre <allanserre@users.noreply.github.com>

--- mafiacuba/src/app/app.module.ts
+++ mafiacuba/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomepageComponent } from './homepage/homepage.component';
+import { Router, RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
