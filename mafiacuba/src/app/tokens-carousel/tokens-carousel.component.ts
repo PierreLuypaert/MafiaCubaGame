15e3b84c15657d116896e5966975ba75672ebed8
allan-serre <allan.serre@etu.univ-lehavre.fr>
2021-06-24 22:52:37 +0200

    add animation and responsive text to carousel

--- mafiacuba/src/app/tokens-carousel/tokens-carousel.component.ts
+++ mafiacuba/src/app/tokens-carousel/tokens-carousel.component.ts
@@ -1,25 +1,18 @@
+import { tokenize } from '@angular/compiler/src/ml_parser/lexer';
 import { getTranslationDeclStmts } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 
-const TOKENS = [
-  "driver-token.png",
-  "fbi-token.png",
-  "loyal-token.png",
-  "driver-token.png",
-  "fbi-token.png",
-  "loyal-token.png",
-  "driver-token.png",
-  "fbi-token.png",
-  "loyal-token.png",
-  "driver-token.png",
-  "fbi-token.png",
-  "loyal-token.png",
-  "driver-token.png",
-  "fbi-token.png",
-  "loyal-token.png",
-  "driver-token.png",
-  "fbi-token.png",
-  "loyal-token.png"
+interface token {
+    link : String
+    name : String
+    desc : String 
+}
+
+const TOKENS : token[] = [
+  { link : "driver-token.png", name : "Conducteur", desc : "Le conducteur soutien le joueur à sa droite"},
+  { link : "fbi-token.png",    name : "FBI"       , desc : "Le FBI doit se faire tuer par le parrain"},
+  { link : "loyal-token.png",  name : "Fidèle"    , desc : "Le fidèle doit aider le parrain à gagner"}
+
 ];
 
 @Component({
@@ -29,30 +22,53 @@ const TOKENS = [
 })
 export class TokensCarouselComponent implements OnInit {
 
-  tokens = TOKENS
+  tokens : token[] = []
+  selectedToken : number = 0
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tokens = this.tokens.concat(TOKENS,TOKENS,TOKENS,TOKENS,TOKENS,TOKENS,TOKENS,TOKENS,TOKENS)
+    this.selectedToken = Math.round(this.tokens.length/2)
   }
-
 
   getTransform( index : number ){
 
     var transform = "transform :"
-    var half = this.tokens.length/2
 
-    if ( index == half )
-      transform += " translate(-50%, -50%) scale(1.6); "
+    if ( index == this.selectedToken )
+      transform += " translate(-50%, -50%) scale(2.2); "
 
-    if ( index < half ) {
-      transform += " translate(calc(-38vh - 50% - (16vh + 20px) *" + (half-index-1) + "),-50%) scale(0.8)"
+    if ( index < this.selectedToken ) {
+      transform += " translate(calc(-38vh - 50% - (16vh + 40px) *" + (this.selectedToken-index-1) + "),-50%) scale(1)"
     }
 
-    if ( index > half ) {
-      transform +=  " translate(calc(38vh - 50% + (16vh + 20px) *" + (index-half-1) + "),-50%) scale(0.8)"
+    if ( index > this.selectedToken ) {
+      transform +=  " translate(calc(38vh - 50% + (16vh + 40px) *" + (index-this.selectedToken-1) + "),-50%) scale(1)"
     }
     
     return transform
   }
+
+  slideToRight(){
+    if ( this.selectedToken - Math.round(this.tokens.length/2) > 4)
+      this.selectedToken =  Math.round(this.tokens.length/2)
+    else
+      this.selectedToken ++;
+  }
+
+  slideToLeft(){
+    if (  Math.round(this.tokens.length/2) - this.selectedToken > 4 )
+      this.selectedToken =  Math.round(this.tokens.length/2)
+    else
+      this.selectedToken --;
+  }
+
+  selectToken( index : number ){
+    this.selectedToken = index
+  }
+
+  get role_name() : String { return this.tokens[this.selectedToken].name }
+  get role_desc() : String { return this.tokens[this.selectedToken].desc }
 }
