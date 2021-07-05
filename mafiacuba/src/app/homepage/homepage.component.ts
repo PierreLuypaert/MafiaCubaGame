bcccbaa31371a4a11f2e89518fdbcd72a1e3657c
allan-serre <allan.serre@etu.univ-lehavre.fr>
2021-07-06 00:59:40 +0200

    merge and repair code

--- mafiacuba/src/app/homepage/homepage.component.ts
+++ mafiacuba/src/app/homepage/homepage.component.ts
@@ -1,56 +1,15 @@
-import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
-import { AfterViewInit, ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
-import { slideInOutAnimation } from '../animations';
-
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss'],
-  
-
-    // make slide in/out animation available to this component
-    animations: [slideInOutAnimation],
-
-    // attach the slide in/out animation to the host (root) element of this component
-    host: { '[@slideInOutAnimation]': '' }
+  styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit, AfterViewInit {
+export class HomepageComponent implements OnInit {
 
+  isConnected : boolean = false;
   
-  imgs : any[] = [
-    { link : "assets/images/news/new1.jpg"},
-    { link : "assets/images/news/new2.jpg"},
-    { link : "assets/images/news/new2.jpg"},
-    { link : "assets/images/news/new1.jpg"},
-  ];
-
-  isOnTop = true;
-  selectedElement : number = 0;
-  autoRadioBtnIndex : number = 0;
-
-  constructor(    
-    private scrollDispatcher: ScrollDispatcher,
-    private zone: NgZone
-    ) { }
-
   ngOnInit(): void {
   } 
 
-  
-  ngAfterViewInit(): void {
-  //  setInterval(() => { this.autoChange() }, 8000);
-  }
-
-  
-  autoChange()
-  {
-    this.autoRadioBtnIndex = ( this.autoRadioBtnIndex >= this.imgs.length-1 ? 0 : this.autoRadioBtnIndex+1);
-    this.change( this.autoRadioBtnIndex);
-  }
-
-  change(index : number)
-  {
-    this.selectedElement = index;
-  }
 }
